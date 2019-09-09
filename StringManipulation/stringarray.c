5366ccc93e88fc52debd152775cbf2e5da13a2b2
rhin123 <ryanrhin@gmail.com>
2019-09-09 17:01:41 -0500

    Fixed invalid int[] to string conversion

--- StringManipulation/stringarray.c
+++ StringManipulation/stringarray.c
@@ -2,58 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+void printIntArray(int* array, int length)
+{
+    int i;
+    printf("[");
+    for (i = 0; i < length; i++) {
+        printf(((i >= length - 1) ? "%d" : "%d,"), array[i]);
+    }
+    printf("]\n");
+}
 
-void printIntArray(int *array, int length) {
-        int i;
-        printf("[");
-        for(i = 0; i < length; i++){
-                char *printFormat = ((i >= length - 1) ? "%d" : "%d,");
-                printf(printFormat, array[i]);
+int* intArrayFromString(char* input)
+{
+    //TODO: Check if we have a valid string format
+    //TODO: Make more effiecent
+    //TODO: Reformat to not look like crap.;
+
+    unsigned long i;
+    unsigned long inputLength = strlen(input);
+    int arrayIndex = 0;
+    char currentNum[256];
+
+    int* array = (int*)malloc(inputLength * sizeof(int));
+
+    for (i = 0; i < inputLength; i++) {
+        if (input[i] != ',')
+            currentNum[strlen(currentNum)] = input[i];
+
+        if (input[i] == ',' || i >= inputLength - 1) {
+            currentNum[strlen(currentNum) + 1] = '\0';
+            array[arrayIndex] = atoi(currentNum);
+            memset(currentNum, 0, strlen(currentNum));
+            arrayIndex++;
         }
-        printf("]\n");
+    }
+    return array;
 }
 
-int *intArrayFromString(char *input){
-        //TODO: Check if we have a valid string format
-        //TODO: Make more effiecent
-        //TODO: Reformat to not look like crap.;
+int arraySizeFromString(char* input)
+{
+    int size = 0;
 
-        int i;
-        int arrayIndex = 0;
-        int inputLength = strlen(input);
-        char currentNum[256];
+    unsigned long i;
+    unsigned long inputLength = strlen(input);
+    for (i = 0; i < inputLength; i++)
+        if (input[i] == ',' || i >= inputLength)
+            size++;
 
-        int *array = (int *) malloc(inputLength * sizeof(int));
-
-        for(i = 0; i < inputLength; i++){
-                if(input[i] == ',')
-                {
-                        currentNum[strlen(currentNum) + 1] = '\0';
-                        array[arrayIndex] = atoi(currentNum);
-                        strcpy(currentNum, "");
-                        arrayIndex++;
-                } else {
-                        currentNum[strlen(currentNum)] = input[i];
-                        if(i >= inputLength - 1)
-                        {
-                                currentNum[strlen(currentNum) + 1] = '\0';
-                                array[arrayIndex] = atoi(currentNum);
-                        }
-
-                }
-        }
-return array;
+    return (size == 0) ? 0 : size + 1;
 }
-
-int arraySizeFromString(char* input){
-        int i;
-        int size = 0;
-        int inputLength = strlen(input);
-        for(i = 0; i < inputLength; i++)
-                if(input[i] == ',' || i >= inputLength)
-                        size++;
-
-        return (size == 0) ? 0 : size + 1;
-}
-
-
