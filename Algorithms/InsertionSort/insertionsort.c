2b64df726144d5a69ee856a3fac05a66f57f70c8
rhin123 <ryanrhin@gmail.com>
2019-09-09 17:03:38 -0500

    Fixed formatting

--- Algorithms/InsertionSort/insertionsort.c
+++ Algorithms/InsertionSort/insertionsort.c
@@ -11,39 +11,45 @@
  *
  *
  */
-int *insertionSort(int array[], int length){
+int* insertionSort(int array[], int length)
+{
 
-	//Start at 0. Check if the elements behind it are greater than the num
+    //Start at 0. Check if the elements behind it are greater than the num
 
-	int i, currentNum, j;
-	for(i = 1; i < length; i++){
+    int i, currentNum, j;
+    for (i = 1; i < length; i++) {
 
-		//Get the current num we want to sort in the array.
-		currentNum = array[i];
-		//Define our starting index. (From high to low).
-		j = i - 1;
+        //Get the current num we want to sort in the array.
+        currentNum = array[i];
+        //Define our starting index. (From high to low).
+        j = i - 1;
 
-		//If our currentNum is still to high, shift down.
-		while(j >= 0 && array[j] > currentNum){
-			//Shift the whole array over to the right.
-			array[j + 1] = array[j];
-			j = j - 1;
-		}
+        //If our currentNum is still to high, shift down.
+        while (j >= 0 && array[j] > currentNum) {
+            //Shift the whole array over to the right.
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
 
-		//Put the current num at our determined index.
-		array[j + 1] = currentNum;
-	}
+        //Put the current num at our determined index.
+        array[j + 1] = currentNum;
+    }
 
-	return array;
+    return array;
 }
 
-int main(int arc, char** argv){
-	//TODO: Im probally gonna make a custom typedef array so i dont have to manually get the size again.
-	int *array = intArrayFromString(argv[1]);
-	int arraySize = arraySizeFromString(argv[1]);
-	printf("Before Sort:\n");
-	printIntArray(array,arraySize);
-	printf("After Sort:\n");
-	printIntArray(insertionSort(array,arraySize), arraySize);	
-	return 0;
+int main(int argc, char** argv)
+{
+    if (argc <= 1) {
+        printf("Error: Enter an array\n");
+        return 1;
+    }
+    //TODO: Im probally gonna make a custom typedef array so i dont have to manually get the size again.
+    int* array = intArrayFromString(argv[1]);
+    int arraySize = arraySizeFromString(argv[1]);
+    printf("Before Sort:\n");
+    printIntArray(array, arraySize);
+    printf("After Sort:\n");
+    printIntArray(insertionSort(array, arraySize), arraySize);
+    return 0;
 }
--- Algorithms/SelectionSort/selectionsort.c
+++ Algorithms/SelectionSort/selectionsort.c
@@ -1,33 +1,41 @@
-#include <stdio.h>
 #include "../../StringManipulation/stringarray.h"
+#include <stdio.h>
 
-void swap(int *x1, int *x2){
-	int temp = *x1;
-	*x1 = *x2;
-	*x2 = temp;
+void swap(int* x1, int* x2)
+{
+    int temp = *x1;
+    *x1 = *x2;
+    *x2 = temp;
 }
 
-int *selectionSort(int array[], int length){
-	int i,j,minNumIndex;
+int* selectionSort(int array[], int length)
+{
+    int i, j, minNumIndex;
 
-	for(i = 0; i < length - 1; i++){
-		minNumIndex = i;
-		for(j = i+1; j < length; j++)
-			if(array[j] < array[minNumIndex])
-				minNumIndex = j;
+    for (i = 0; i < length - 1; i++) {
+        minNumIndex = i;
+        for (j = i + 1; j < length; j++)
+            if (array[j] < array[minNumIndex])
+                minNumIndex = j;
 
-		swap(&array[minNumIndex],&array[i]);
-	}
+        swap(&array[minNumIndex], &array[i]);
+    }
 
-	return array;
+    return array;
 }
 
-int main(int argc, char** argv){
-	int *array = intArrayFromString(argv[1]);
-	int arraySize = arraySizeFromString(argv[1]);
-	printf("Before Sort:\n");
-	printIntArray(array,arraySize);
-	printf("After Sort:\n");
-	printIntArray(selectionSort(array,arraySize), arraySize);	
-}
+int main(int argc, char** argv)
+{
+    if (argc <= 1) {
+        printf("Error: Enter an array\n");
+        return 1;
+    }
+    int* array = intArrayFromString(argv[1]);
+    int arraySize = arraySizeFromString(argv[1]);
+    printf("Before Sort:\n");
+    printIntArray(array, arraySize);
+    printf("After Sort:\n");
+    printIntArray(selectionSort(array, arraySize), arraySize);
 
+    return 0;
+}
